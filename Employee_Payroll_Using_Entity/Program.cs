cad8f75c6b9c7b9ed11c8c2581c9ed9cb9f64241
Mahesh7614 <waghmaremahesh012@gmail.com>
2023-02-16 16:31:27 +0530

    logger

--- Employee_Payroll_Using_Entity/Program.cs
+++ Employee_Payroll_Using_Entity/Program.cs
@@ -1,11 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Employee_Payroll_Using_Entity
 {
@@ -13,7 +10,21 @@ namespace Employee_Payroll_Using_Entity
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+            logger.Debug("in it main");
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Program Stopped Beacuse of Exception");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
